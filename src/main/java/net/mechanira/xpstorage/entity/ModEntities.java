1eb745b1135deeac64c125899c8228ea6e1d6b86
Kira <marklegend293@gmail.com>
2025-03-12 15:34:31 +0200

    Upgraded to 1.21.4

---
+++ src/main/java/net/mechanira/xpstorage/entity/ModEntities.java
@@ -0,0 +1,14 @@
+package net.mechanira.xpstorage.entity;
+
+import net.mechanira.xpstorage.XPStorage;
+import net.mechanira.xpstorage.entity.custom.SuperXPBottleEntity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+public class ModEntities {
+    public static void registerModEntities() {
+        XPStorage.LOGGER.info("Registering Mod Entities for " + XPStorage.MOD_ID);
+    }
+}
--- src/main/java/net/mechanira/xpstorage/entity/custom/SupremeXPBottleEntity.java
+++ src/main/java/net/mechanira/xpstorage/entity/custom/SupremeXPBottleEntity.java
@@ -1,11 +1,13 @@
 package net.mechanira.xpstorage.entity.custom;
 
+import net.mechanira.xpstorage.item.ModItems;
 import net.minecraft.component.type.PotionContentsComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ExperienceOrbEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.thrown.ThrownItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.potion.Potions;
 import net.minecraft.server.world.ServerWorld;
@@ -14,16 +16,16 @@ import net.minecraft.world.World;
 import net.minecraft.world.WorldEvents;
 
 public class SupremeXPBottleEntity extends ThrownItemEntity {
-    public SupremeXPBottleEntity(EntityType<? extends SupremeXPBottleEntity> entityType, World world) {
+    public SupremeXPBottleEntity(EntityType<? extends SuperXPBottleEntity> entityType, World world) {
         super(entityType, world);
     }
 
-    public SupremeXPBottleEntity(World world, LivingEntity owner) {
-        super(EntityType.EXPERIENCE_BOTTLE, owner, world);
+    public SupremeXPBottleEntity(World world, LivingEntity owner, ItemStack stack) {
+        super(EntityType.EXPERIENCE_BOTTLE, owner, world, stack);
     }
 
-    public SupremeXPBottleEntity(World world, double x, double y, double z) {
-        super(EntityType.EXPERIENCE_BOTTLE, x, y, z, world);
+    public SupremeXPBottleEntity(World world, double x, double y, double z, ItemStack stack) {
+        super(EntityType.EXPERIENCE_BOTTLE, x, y, z, world, stack);
     }
 
     @Override
@@ -40,7 +42,7 @@ public class SupremeXPBottleEntity extends ThrownItemEntity {
     protected void onCollision(HitResult hitResult) {
         super.onCollision(hitResult);
         if (this.getWorld() instanceof ServerWorld) {
-            this.getWorld().syncWorldEvent(WorldEvents.SPLASH_POTION_SPLASHED, this.getBlockPos(), PotionContentsComponent.getColor(Potions.WATER));
+            this.getWorld().syncWorldEvent(WorldEvents.SPLASH_POTION_SPLASHED, this.getBlockPos(), -13083194);
             int i = 255 + this.getWorld().random.nextInt(412) + this.getWorld().random.nextInt(413);
             ExperienceOrbEntity.spawn((ServerWorld)this.getWorld(), this.getPos(), i);
             this.discard();
